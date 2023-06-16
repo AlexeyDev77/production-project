7ececbede2bf4a48a3be322077c13d8247cf7ac3
Abroskin.callkeeper.ru <aa@callkeeper.ru>
2023-06-16 12:34:37 +0300

    Add jest and first test.

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,37 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass'))
+            .toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass cls1 cls2';
+        expect(classNames('someClass', {}, ['cls1', 'cls2']))
+            .toBe(expected);
+    });
+    test('with mods', () => {
+        const expected = 'someClass cls1 cls2 class3 class4';
+        expect(classNames(
+            'someClass',
+            { class3: true, class4: true },
+            ['cls1', 'cls2'],
+        )).toBe(expected);
+    });
+    test('with mods false', () => {
+        const expected = 'someClass cls1 cls2 class3';
+        expect(classNames(
+            'someClass',
+            { class3: true, class4: false },
+            ['cls1', 'cls2'],
+        )).toBe(expected);
+    });
+    test('with mods undefined', () => {
+        const expected = 'someClass cls1 cls2 class3';
+        expect(classNames(
+            'someClass',
+            { class3: true, class4: undefined },
+            ['cls1', 'cls2'],
+        )).toBe(expected);
+    });
+});
