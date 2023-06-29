a2c376477f7a12c2996c7a3298591898fe3c9e2e
Abroskin.callkeeper.ru <aa@callkeeper.ru>
2023-06-29 11:39:25 +0300

    Add checks and fix dev plugins.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,14 +18,14 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
 
     if (isDev) {
         plugins.push(new ReactRefreshPlugin());
         plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
     }
 
     return plugins;
