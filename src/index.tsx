73801735264ddcb8bea09f4d116db180cac061f9
AlexeyDev77 <lexa.abroskin@gmail.com>
2023-07-17 16:54:05 +0300

    Fix and test CI.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
-import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
+import './shared/config/i18n/i18n';
 import 'app/styles/index.scss';
 
 render(
